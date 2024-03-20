b03dd9c216e4a38871e2b11bef71b403e955d907
alieftwu <alieftwu@gmail.com>
2024-03-19 23:20:19 -0500

    Added walking script

---
+++ Game Jam/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed; 
+    private Rigidbody2D rb;
+    private Vector2 movInput;
+    public Animator animator;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        animator.SetFloat("Horizontal", movInput.x);
+        animator.SetFloat("Vertical", movInput.y);
+        animator.SetFloat("Speed", movInput.sqrMagnitude);
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = movInput * speed;
+        
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        movInput = inputValue.Get<Vector2>();
+    }
+}
