0154286073299e566e285d006e05f42339480cae
alieftwu <alieftwu@gmail.com>
2024-03-22 21:32:59 -0500

    Added very basic win/lose state screens
    Added functionality to where if the player has required amount of votes, it goes to win state, and vice versa for lose state

---
+++ Game Jam/Assets/WinLoseTrigger.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class WinLoseTrigger : MonoBehaviour
+{
+
+   public int votesRequired = 4; // Number of votes required to win
+    private string sceneToLoad;
+    private bool enterAllowed;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("entered trigger");
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("comparing player working");
+            int votesCollected = DialogueManager.GetInstance().votesCollected; // Get the number of votes collected
+            
+                if (votesCollected >= votesRequired)
+                {   
+                    Debug.Log("win state");
+                    sceneToLoad = "Win Screen"; // Load win state scene
+                    enterAllowed = true;
+                }
+                else
+                {
+                    Debug.Log("lose state");
+                    sceneToLoad = "Lose Screen"; // Load lose state scene
+                    enterAllowed = true;
+                }
+                
+            
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("player exit trigger");
+        enterAllowed = false;
+    }
+
+    private void Update()
+    {
+        if(enterAllowed && Input.GetKey(KeyCode.Return))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
